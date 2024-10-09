6809113e431fe94202c9ce1f28247d75b8817098
Scott Tolinski <scott.tolinski@gmail.com>
2024-10-09 09:06:02 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Zero Svelte
+
+Zero is the local first platform for building incedible, super fast apps.
+
+To use Zero Svelte, you need to follow the Zero docs to get started.
+
+## Install
+
+## Usage
---
+++ src/lib/query.svelte.ts
@@ -0,0 +1,26 @@
+import type { Query as QueryParam, QueryImpl, QueryType, Smash, TableSchema } from '@rocicorp/zero';
+
+export class Query<TSchema extends TableSchema, TReturn extends QueryType> {
+	#queryImpl: QueryImpl<TSchema, TReturn>;
+	data: Smash<TReturn> | undefined = $state();
+
+	constructor(q: QueryParam<TSchema, TReturn>, enable: boolean = true) {
+		this.#queryImpl = q as unknown as QueryImpl<TSchema, TReturn>;
+		this.data = this.#queryImpl.singular ? undefined : ([] as unknown as Smash<TReturn>);
+		$effect(() => {
+			if (enable) {
+				const view = this.#queryImpl.materialize();
+				const unsubscribe = view.addListener((snap: Smash<TReturn> | undefined) => {
+					this.data = snap === undefined ? snap : ($state.snapshot(snap) as Smash<TReturn>);
+				});
+				view.hydrate();
+				return () => {
+					unsubscribe();
+					view.destroy();
+				};
+			}
+			this.data = (this.#queryImpl.singular ? undefined : []) as unknown as Smash<TReturn>;
+			return () => {};
+		});
+	}
+}

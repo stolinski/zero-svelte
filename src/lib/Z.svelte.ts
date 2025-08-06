068d01cb83aeb695ffda70f281e67ab1c5e510ea
Cory Tyburski <corytyburski@gmail.com>
2025-08-06 13:16:10 -0400

    Add online status to be accessible

--- src/lib/Z.svelte.ts
+++ src/lib/Z.svelte.ts
@@ -1,4 +1,4 @@
-import { Zero, type Schema, type ZeroOptions, type CustomMutatorDefs } from '@rocicorp/zero';
+import { Zero, type CustomMutatorDefs, type Schema, type ZeroOptions } from '@rocicorp/zero';
 
 // This is the state of the Zero instance
 // You can reset it on login or logout
@@ -7,17 +7,31 @@ export class Z<
 	MD extends CustomMutatorDefs<TSchema> | undefined = undefined
 > {
 	current: Zero<TSchema, MD> = $state(null!);
+	online = $state<boolean>();
+	#onlineUnsubscribe: (() => void) | undefined;
 
 	constructor(z_options: ZeroOptions<TSchema, MD>) {
 		this.build(z_options);
 	}
 
 	build(z_options: ZeroOptions<TSchema, MD>) {
+		// Clean up previous subscription if it exists
+		this.#onlineUnsubscribe?.();
+
 		// Create new Zero instance
 		this.current = new Zero(z_options);
+
+		// Initialize online status
+		this.online = true;
+
+		// Subscribe to online status changes
+		this.#onlineUnsubscribe = this.current.onOnline((online) => {
+			this.online = online;
+		});
 	}
 
 	close() {
+		this.#onlineUnsubscribe?.();
 		this.current.close();
 	}
 }

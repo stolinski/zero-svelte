7647d56028156832c1f3ce95498dd0825293cab1
Robert Wettstädt <robert@wettstaedt.dev>
2025-09-06 13:33:17 +0200

    Update zero to 23 and support Synced Queries

---
+++ src/routes/api/get-queries/+server.ts
@@ -0,0 +1,27 @@
+import { withValidation, type ReadonlyJSONValue } from '@rocicorp/zero';
+import { handleGetQueriesRequest } from '@rocicorp/zero/server';
+import { queries, schema } from '../../../schema.js';
+
+export async function POST({ request }) {
+	const q = await handleGetQueriesRequest(getQuery, schema, request);
+
+	return new Response(JSON.stringify(q));
+}
+
+// Build a map of queries with validation by name.
+const validated = Object.fromEntries(
+	Object.values(queries).map((q) => [q.queryName, withValidation(q)])
+);
+
+function getQuery(name: string, args: readonly ReadonlyJSONValue[]) {
+	const q = validated[name];
+	if (!q) {
+		throw new Error(`No such query: ${name}`);
+	}
+	return {
+		// First param is the context for contextful queries.
+		// `args` are validated using the `parser` you provided with
+		// the query definition.
+		query: q(undefined, ...args)
+	};
+}
